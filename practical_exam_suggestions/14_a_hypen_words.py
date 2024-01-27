b59f0b9df891c513a883280d81844c48cbae19bf
abhraneel2004 <contact.abhraneel@gmail.com>
2024-01-28 01:13:21 +0530

    Files Added

---
+++ practical_exam_suggestions/14_a_hypen_words.py
@@ -0,0 +1,6 @@
+
+s = input("Enter the hypen separated string:")
+l = s.split("-")
+l.sort()
+s2 = "-".join(l)
+print(s2)
---
+++ practical_exam_suggestions/14_b_prime.py
@@ -0,0 +1,15 @@
+def primenum(n):
+    if n==0 or n==1 or (n>2 and n%2==0):
+        return False
+    if n==2:
+        return True
+    for i in range(3,int(n**0.5),2):
+        if (n%i==0):
+            return False
+    return True
+
+n = int(input("Enter a number: "))
+if (primenum(n)):
+    print(f"{n} is a Prime number")
+else:
+    print(f"{n} is not a Prime number")
