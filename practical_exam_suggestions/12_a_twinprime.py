4eb08097ba46a042c1b32370d4b4e877a25fc6ae
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-01 00:15:19 +0530

    Files Edited

--- practical_exam_suggestions/12_a_twinprime.py
+++ practical_exam_suggestions/12_a_twinprime.py
@@ -1,10 +1,9 @@
 def primenum(n):
+    if n==0 or n==1 or (n>2 and n%2==0):
+        return False
     if n==2:
         return True
-    if n==0 or n==1 or n%2==0:
-        return False
-    
-    for i in range(3,int(n**0.5),2):
+    for i in range(3,int(n**0.5)+1,2):
         if (n%i==0):
             return False
     return True
--- practical_exam_suggestions/14_b_prime.py
+++ practical_exam_suggestions/14_b_prime.py
@@ -3,7 +3,7 @@ def primenum(n):
         return False
     if n==2:
         return True
-    for i in range(3,int(n**0.5),2):
+    for i in range(3,int(n**0.5)+1,2):
         if (n%i==0):
             return False
     return True
