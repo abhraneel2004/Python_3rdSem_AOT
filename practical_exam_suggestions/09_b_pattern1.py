b1bf4e82522b456f067fc3b10593107cc02e49d2
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-03 23:29:25 +0530

    File Modified

--- practical_exam_suggestions/09_b_pattern1.py
+++ practical_exam_suggestions/09_b_pattern1.py
@@ -1,5 +1,15 @@
+'''
+1 
+2 1 
+4 2 1 
+8 4 2 1 
+16 8 4 2 1 
+
+'''
+
 n = int(input("Enter the number of rows: "))
 for i in range(0,n+1):
     for j in range(i,-1,-1):
         print((2**j), end = " ")
     print()
+
