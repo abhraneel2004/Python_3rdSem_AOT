defac92061d4e3a0b04fc9da852983e5993464f3
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-05 23:37:19 +0530

    File Modified

--- 32_list_addition.py
+++ 32_list_addition.py
@@ -1,18 +1,12 @@
 n = int(input("Enter the no. of elements: "))
 l1,l2 = [],[]
-l3 = []
 
 print("List 1: ")
 for i in range(n):
-	x = int(input("Enter the element: "))
-	l1.append(x)
+	l1.append(int(input("Enter the element: ")))
 
 print("List 2: ")
 for i in range(n):
-	x = int(input("Enter the element: "))
-	l2.append(x)
+	l2.append(int(input("Enter the element: ")))
 	
-for i in range(n):
-	l3.append(l1[i]+l2[i])
-
-print(l3)
+l1 = l1+l2
