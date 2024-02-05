dc9e830be1228699bf1acec3f146a5e4721dbc0b
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-05 23:17:17 +0530

    File Modified

--- 29_list_common_element.py
+++ 29_list_common_element.py
@@ -1,10 +1,11 @@
 def common(l1,l2):
 	l1,l2 = set(l1), set(l2)
-	if len(l1.intersection(l2))==0:
-		return False
-	return True
+	return list(l1.intersection(l2))
 	
 l1 = eval(input("Enter list 1: "))
 l2 = eval(input("Enter list 2: "))
-print(common(l1,l2))
-
+l = common(l1,l2)
+if(l):
+	print("Common Elements: ",l)
+else:
+	print("Nothing in common")
