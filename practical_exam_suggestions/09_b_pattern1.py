bd34e899b2751427882a49ae3471875b29e8dc91
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-05 22:45:11 +0530

    File Modified

--- practical_exam_suggestions/09_b_pattern1.py
+++ practical_exam_suggestions/09_b_pattern1.py
@@ -13,3 +13,13 @@ for i in range(0,n+1):
         print((2**j), end = " ")
     print()
 
+'''
+n = 5
+i -> 0,1,2,3,4,5
+j -> 1,0
+range(1,5) 1,2,3,4
+range(5,1) X
+range(1,1) X
+range(5,-1,-1) 5,4,3,2,1,0
+'''
+
