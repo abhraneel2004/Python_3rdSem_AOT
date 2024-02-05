48e4904e26bec38d6affc15ce7c7f0ac34130add
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-06 00:44:41 +0530

    Files Added

---
+++ practical_exam_suggestions/19_a_primeFactors.py
@@ -0,0 +1,16 @@
+
+def primenum(n):
+    if n == 0 or n == 1 or (n > 2 and n % 2 == 0):
+        return False
+    if n == 2:
+        return True
+    for i in range(3, int(n**0.5)+1, 2):
+        if (n % i == 0):
+            return False
+    return True
+
+
+n = int(input("Enter the number: "))
+for i in range(2, n+1):
+    if n % i == 0 and primenum(i):
+        print(i, end=" ")
